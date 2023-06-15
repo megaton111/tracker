56628b0781304c01b590e289de065b1dbdef58b0
keunholee <lkh890111@gmail.com>
2023-06-15 14:07:26 +0900

    전화번호 - 제거 수정

--- src/components/common/TelChange.vue
+++ src/components/common/TelChange.vue
@@ -1,51 +1,27 @@
 <template>
-  <div><t-input type="text" v-model="inputValue" ref="inputTarget"></t-input></div>
+  <div><t-input type="text" v-model="inputValue"></t-input></div>
   <div class="flex items-center gap-1 mt-1" v-if="changeTel">
     <div id="textWrap" class="dark:text-gray-100">{{ changeTel }}</div>
     <button 
         type="button" 
         class="inline-flex justify-center px-2 py-1 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        @click="copyHandler('#textWrap'); open = false;"
+        @click="copyHandler('#textWrap');"
       >복사</button>
   </div>
-  <!-- <div><t-input type="text" v-model="telInput" @input="changeHandler" ref="inputTarget"></t-input></div> -->
-  <!-- <div><t-input type="text" v-model="inputValue" @update:model-value="$emit('update:modelValue', $event)" ref="inputTarget"></t-input></div> -->
 </template>
 
 <script>
   import { reactive, toRefs, ref, computed } from 'vue';
-  import { priceComma } from '@/utils';
   import { copyText } from '@/utils';
-
   export default {
     name : 'TelChange' ,
-    components : {
-    } , 
-    props : {
-    } ,
+    components : {} , 
+    props : {} ,
     setup () {
-
-      const inputTarget = ref( null ) ; 
 
       const state = reactive({
         inputValue : '' ,
       }) ;
-
-      const addOption = () => {
-        let count = state.options.length+1 ;
-        state.options.push({ name : '옵션 '+ count, value : 0 })
-      }
-
-      const changeNum = ( num ) => priceComma( parseFloat( num ).toFixed() ) ; 
-
-      const resetHandler = () => {
-        state.priceOrigin = 0 ;
-        state.options = [{ name : '옵션 1' , value : 0 }] ;
-      }
-
-      const changeHandler = () => {
-        state.inputValue = state.inputValue.replace(/-/gi, "").trim();
-      }
 
       const copyHandler = ( target ) => {
         let copyTarget = document.querySelector( target ) ; 
@@ -53,17 +29,10 @@
         state.inputValue = ''  ;
       } ;
 
-      const changeTel = computed(() => {
-        return state.inputValue.replace(/-/gi, "").trim();
-      }) ;
+      const changeTel = computed(() => state.inputValue.replace(/-/gi, "").trim()) ;
       
       return { 
         ...toRefs(state) ,
-        addOption , 
-        changeNum , 
-        resetHandler , 
-        changeHandler ,
-        inputTarget ,
         changeTel ,
         copyHandler
       } ;
@@ -73,6 +42,5 @@
 
   }
 </script>
-
 <style lang="scss" scoped>
 </style>
