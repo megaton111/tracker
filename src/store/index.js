f29cf30416fb1dadde3b9329fc52db7a977779f1
keunholee <lkh890111@gmail.com>
2023-07-31 08:29:48 +0900

    배송 API 업데이트

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ export default createStore({
   strict: false,
   state: {
     deliveryInfo : {
-      APIKEY : 'MytdaG6kU86HoUjjgdnyYQ' ,
+      APIKEY : '27c7c716GbkQsGgkMcj4YQ' ,
     } ,
     exchangeInfo : {
       APIKEY : 'h230q242y019c270i090x040r0' ,
